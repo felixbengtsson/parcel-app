bcaa594f0ee3fc38ab983f55970816308b19928c
Felix Bengtsson <felix.bengtsson@me.com>
2018-08-22 23:53:26 +0200

    made it work

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 
-export default class componentName extends Component {
+class App extends Component {
   render() {
     return (
       <div>
@@ -10,3 +10,5 @@ export default class componentName extends Component {
     );
   }
 }
+
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
   return (
     <div>
-      <h1>header</h1>
+      <nav className="navigation" />
     </div>
   );
 };
