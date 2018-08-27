2906ce9922eb95b15a3cb2fa85bf8bd2d3fb8634
Felix Bengtsson <felix.bengtsson@me.com>
2018-08-27 23:36:41 +0200

    loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import jump from "../images/nora_jump.jpg";
 import Loading from "./LoadingHOC";
 
-const Home = () => {
-  return (
-    <div className="home">
-      <div className="home__welcome">
-        <h1>Hello</h1>
-        <p>
-          Hi! I՚m Nora, a freelance dancer, singer and perfomer working in
-          Stockholm & London. Check out my work.
-        </p>
-      </div>
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true
+    };
+  }
 
-      <div className="home__hero">
-        <div className="bg">
-          <img src={jump} alt="" />
+  componentDidMount() {
+    this.setState({ loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      console.log("loading");
+      return null;
+    } else {
+      return (
+        <div className="home">
+          <div className="home__welcome">
+            <h1>Hello</h1>
+            <p>
+              Hi! I՚m Nora, a freelance dancer, singer and perfomer working in
+              Stockholm & London. Check out my work.
+            </p>
+          </div>
+
+          <div className="home__hero">
+            <div className="bg">
+              <img src={jump} alt="" />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
-};
+      );
+    }
+  }
+}
 
-export default Loading(Home);
+export default Home;
